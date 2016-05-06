7cae5583cf5f328b08d5aaa653010d4692e66449
Peni <smallr@gmail.com>
2016-05-06 23:27:37 +0800

    Add photo view activity.

--- app/src/main/java/com/jason/minniejasonwedding/MainActivity.java
+++ app/src/main/java/com/jason/minniejasonwedding/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jason.minniejasonwedding;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onPhotoViewButtonClick(View v) {
+        Intent intent = new Intent(this, PhotoViewActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/jason/minniejasonwedding/PhotoViewActivity.java
@@ -0,0 +1,123 @@
+package com.jason.minniejasonwedding;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import uk.co.senab.photoview.PhotoView;
+
+public class PhotoViewActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_view);
+
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        setContentView(mViewPager);
+        mViewPager.setAdapter(new SamplePagerAdapter());
+    }
+
+    static class SamplePagerAdapter extends PagerAdapter {
+
+        private static final int[] sDrawables = {
+                R.drawable.ds_18327,
+                R.drawable.ds_18334,
+                R.drawable.ds_18336,
+                R.drawable.ds_18337,
+                R.drawable.ds_18357,
+                R.drawable.ds_18358,
+                R.drawable.ds_18362,
+                R.drawable.ds_18365,
+                R.drawable.ds_18372,
+                R.drawable.ds_18376,
+                R.drawable.ds_18380,
+                R.drawable.ds_18413,
+                R.drawable.ds_18415,
+                R.drawable.ds_18422,
+                R.drawable.ds_18430,
+                R.drawable.ds_18433,
+                R.drawable.ds_18435,
+                R.drawable.ds_18443,
+                R.drawable.ds_18445,
+                R.drawable.ds_18446,
+                R.drawable.ds_18459,
+                R.drawable.ds_18465,
+                R.drawable.ds_18468,
+                R.drawable.ds_18469,
+                R.drawable.ds_18470,
+                R.drawable.ds_18485,
+                R.drawable.ds_18492,
+                R.drawable.ds_18500,
+                R.drawable.ds_18501,
+                R.drawable.ds_18509,
+                R.drawable.ds_18519,
+                R.drawable.ds_18520,
+                R.drawable.ds_18527,
+                R.drawable.ds_18530,
+                R.drawable.ds_18532,
+                R.drawable.ds_18540,
+                R.drawable.ds_18563,
+                R.drawable.ds_18565,
+                R.drawable.ds_18567,
+                R.drawable.ds_18573,
+                R.drawable.ds_18574,
+                R.drawable.ds_18575,
+                R.drawable.ds_18576,
+                R.drawable.ds_18578,
+                R.drawable.ds_18584,
+                R.drawable.ds_18586,
+                R.drawable.ds_18590,
+                R.drawable.ds_18591,
+                R.drawable.ds_18606,
+                R.drawable.ds_18608,
+                R.drawable.ds_18621,
+                R.drawable.ds_18627,
+                R.drawable.ds_18640,
+                R.drawable.ds_18646,
+                R.drawable.ds_18650,
+                R.drawable.ds_18684,
+                R.drawable.ds_18698,
+                R.drawable.ds_18699,
+                R.drawable.ds_18723,
+                R.drawable.ds_18726,
+                R.drawable.ds_18736,
+                R.drawable.ds_18739,
+                R.drawable.ds_18760,
+                R.drawable.ds_18764,
+                R.drawable.ds_18765,
+        };
+
+        @Override
+        public int getCount() {
+            return sDrawables.length;
+        }
+
+        @Override
+        public View instantiateItem(ViewGroup container, int position) {
+            PhotoView photoView = new PhotoView(container.getContext());
+            photoView.setImageResource(sDrawables[position]);
+
+            // Now just add PhotoView to ViewPager and return it
+            container.addView(photoView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+            return photoView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+    }
+}
